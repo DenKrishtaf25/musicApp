d04c91c375449e6f93f1086b957542b2671a680f
Denis <dkrishtaf98@gmail.com>
2020-12-27 01:11:08 +0300

    player

---
+++ src/components/Player.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPlay,
+    faAngleLeft,
+    faAngleRight,
+    faPause
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = ({
+    audioRef,
+    currentSong,
+    isPlaying,
+    setIsPlaying,
+    setSongInfo,
+    songInfo,
+    songs,
+    setCurrentSong,
+    setSongs
+}) => {
+
+    const activeLibraryHandler = (nextPrev) =>{
+        const newSongs = songs.map((song) => {
+            if (song.id === nextPrev.id) {
+                return {
+                    ...song,
+                    active: true,
+                };
+            } else {
+                return {
+                    ...song,
+                    active: false,
+                };
+            }
+        });
+        setSongs(newSongs);
+    }
+   
+
+
+    //Events Handlers
+    const playSongHandler = () => {
+        if (isPlaying) {
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        } else {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+    };
+
+    const getTime = (time) => {
+        return (
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        );
+    };
+
+    const skipTrackHandler = async (direction) => {
+        let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+        if (direction === "skip-forward") {
+            await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+            activeLibraryHandler(songs[(currentIndex + 1) % songs.length])
+        }
+        if (direction === "skip-back") {
+            if ((currentIndex - 1) % songs.length === -1) {  /* Если индекс становится -1 ,то  */
+                await setCurrentSong(songs[songs.length - 1]);    /* текущая песня это с индексом который равен длине массива -1 (т.к массив начинается с 0 , то это будет последний индекс) */
+                activeLibraryHandler(songs[songs.length - 1]);
+                if (isPlaying) audioRef.current.play();
+                return; // возвращаемся в начало чтобы то что написано ниже выполнялось не сразу после выполнения ифа выше
+            }
+            await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+            activeLibraryHandler(songs[(currentIndex - 1) % songs.length]);
+        }
+        if (isPlaying) audioRef.current.play();
+    };
+
+    //add the styles
+    const trackAnim = {
+        transform: `translateX(${songInfo.animationPercentage}%)`
+    }
+
+
+    const dragHandler = (e) => {
+        audioRef.current.currentTime = e.target.value;
+        setSongInfo({ ...songInfo, currentTime: e.target.value });
+    }
+
+
+    return (
+        <div className="player">
+            <div className="player__track">
+                <p>{getTime(songInfo.currentTime)}</p>
+                <div className="track">
+                    <input
+                        style={{ background: `linear-gradient(to right, ${currentSong.color[0]},${currentSong.color[1]})` }}
+                        min={0}
+                        max={songInfo.duration || 0}
+                        value={songInfo.currentTime}
+                        onChange={dragHandler}
+                        type="range"
+                    />
+                    <div style={trackAnim} className="animate-track"></div>
+                </div>
+                <p>{songInfo.duration ? getTime(songInfo.duration) : "жопа"}</p>
+            </div>
+            <div className="player__control">
+                <FontAwesomeIcon onClick={() => skipTrackHandler('skip-back')} className="skip-back" size="2x" icon={faAngleLeft} />
+                <FontAwesomeIcon
+                    onClick={playSongHandler}
+                    className="play"
+                    size="2x"
+                    icon={isPlaying ? faPause : faPlay} />
+                <FontAwesomeIcon onClick={() => skipTrackHandler('skip-forward')} className="skip-forward" size="2x" icon={faAngleRight} />
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Player;
---
+++ src/data.js
@@ -0,0 +1,124 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+    return [
+        {
+            name: "My Babylon",
+            artist: "Andy Panda (Endspiel)",
+            audio: "https://dl2.mp3party.net/online/5863155.mp3",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/8404541e3b694d16fd79433b142ed910f36764dd-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: true
+        },
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11768",
+            cover: "https://chillhop.com/wp-content/uploads/2020/05/16bcbe48680098798af04f176daae3693839238d-1024x1024.jpg",
+            color: ["#7D8EC1", "#CB9BC1"],
+            id: uuidv4(),
+            active: false
+        },
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11770",
+            cover: "https://chillhop.com/wp-content/uploads/2020/04/478079cd16fca3ef62a4667dff1673e96e2b635f-1024x1024.jpg",
+            color: ["#CB7471", "#6597B8"],
+            id: uuidv4(),
+            active: false
+        },
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9331",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+            color: ["#1A4E97", "#B5766E"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=8127",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/43e472df5f7dfe5ad546f94b722fa3ca401eb138-1024x1024.jpg",
+            color: ["#F4695F", "#DBA460"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9923",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/50758f405d2aba9cd6e8c4e6cfce44e4d9893714-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9307",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/9630fcab71c6dc1e9ad8140a03dcab54b8b98098-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://dl2.mp3party.net/online/5863155.mp3",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/d019a313218305f3629dcfe384a0de95da01267a-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/bc9a7af2ad67de703541c2c7a91e3df74ab494c0-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10255",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/58028b4ad62050072228e4c833916e5512772399-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=8036",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/b162505d856f1b7326401a00d716f48be4529ed7-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false
+        }
+        ,
+        {
+            name: "Мой Babylon",
+            artist: "Эндшпиль",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9921",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/b2e2c1fca3e00a74402969e0a9e98d4849f38aef-1024x1024.jpg",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false
+        }
+    ]
+}
+
+export default chillHop;
